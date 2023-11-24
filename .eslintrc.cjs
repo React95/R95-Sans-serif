7d4993e6d84c778c098435f3ac9075e58d710651
ggdaltoso <ggdaltoso@gmail.com>
2023-11-23 21:37:23 -0300

    chore: update lint fast-refresh rule drop it

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,12 +11,6 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
